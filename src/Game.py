c5712c80150e0b7e7860a0fbe9506bf6dbedbc5b
Nerea Bejar <NBM883@hotmail.com>
2018-12-04 18:58:53 +0100

    Ranking lecture and drawing done
    The next step will be write on the files. That is almost done, only have to figure out how to input text while it appears on the game screen

--- src/Game.py
+++ src/Game.py
@@ -13,12 +13,14 @@ WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 YELLOW = (255, 255, 0)
+menu_left_margin = 180
 
 #pygame.font.init()
 #myfont = pygame.font.SysFont('8-Bit Madness', 50)
 
 class Game:
-    max_options=3
+    max_options = 3
+    top_ranking = 3
     
     def __init__(self):
         self.font = pygame.font.SysFont('8-Bit Madness', 50)
@@ -26,6 +28,10 @@ class Game:
         self.option = 1
     
     def show_start_screen(self, screen):
+        """
+            Method that displays the start screen of the game
+                :param screen --> Object display where the start screen will be display on
+        """
         screen.fill(BLACK)
         #self.text_surface = myfont.render('Our Pygame', False, WHITE)
         img_ini = pygame.image.load("start_screen_game.jpg")
@@ -37,10 +43,15 @@ class Game:
         return self.wait_for_key_start()
     
     def show_over_screen(self, screen, score):
+        """
+                Method that displays the geame over screen of the game and update the ranking
+                :param  screen  --> Object display where the start screen will be display on
+                        score --> score of the current game to be store
+        """
         print("Final score:")
         print(score)
         screen.fill(BLACK)
-        self.draw_text("GAME OVER", 22, RED, WIDTH/4, HEIGHT/2, screen, True)
+        self.draw_text("GAME OVER", RED, WIDTH/4, HEIGHT/2, screen, True)
         pygame.display.flip()
         return self.wait_for_key_start()
     
@@ -56,15 +67,15 @@ class Game:
     
     def tutorial(self, screen):
         screen.fill(BLACK)
-        self.draw_text("TUTORIAL", 100, RED, WIDTH / 8, HEIGHT / 5, screen, True)
+        self.draw_text("TUTORIAL", RED, WIDTH / 8, HEIGHT / 5, screen, True)
         img1 = pygame.image.load("arrows2.png")
         img2 = pygame.image.load("arrows.jpg")
         screen.blit(img1, (60, HEIGHT/3))
         screen.blit(img2, (60 + 70, HEIGHT / 3))
-        self.draw_text("Arrows - Move the hero", 40, WHITE, WIDTH / 4, HEIGHT / 3, screen, False)
-        self.draw_text("Mouse pointer - Rotate the hero", 40, WHITE, WIDTH / 4, HEIGHT / 3 + 100, screen, False)
-        self.draw_text("Right mouse click - Shoot", 40, WHITE, WIDTH / 4, HEIGHT / 3 + 200, screen, False)
-        self.draw_text("Press <- to go back to the menu", 40, YELLOW, WIDTH / 5, HEIGHT / 3 + 300, screen, False)
+        self.draw_text("Arrows - Move the hero", WHITE, WIDTH / 4, HEIGHT / 3, screen, False)
+        self.draw_text("Mouse pointer - Rotate the hero", WHITE, WIDTH / 4, HEIGHT / 3 + 100, screen, False)
+        self.draw_text("Right mouse click - Shoot", WHITE, WIDTH / 4, HEIGHT / 3 + 200, screen, False)
+        self.draw_text("Press <- to go back to the menu", YELLOW, WIDTH / 5, HEIGHT / 3 + 300, screen, False)
         pygame.display.flip()
         while True:
             for event in pygame.event.get():
@@ -81,47 +92,90 @@ class Game:
     
     def ranking(self,screen):
         screen.fill(BLACK)
-        self.draw_text("Ranking", 100, RED, WIDTH / 8, HEIGHT / 5, screen, True)
-        self.draw_text("Press <- to go back to the menu", 40, YELLOW, WIDTH / 5, HEIGHT / 3 + 300, screen, False)
+        self.draw_text("Ranking", RED, WIDTH / 8, HEIGHT / 5, screen, True)
+        self.ranking_draw(screen)
+        self.draw_text("Press <- to go back to the menu", YELLOW, WIDTH / 5, HEIGHT / 3 + 300, screen, False)
         pygame.display.flip()
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()  # ends pygame
-                    os.exit(0)
+                    os._exit(0)
                     sys.exit()  # ends the program
                     return
                 if event.type == pygame.KEYDOWN:
                     if event.key == K_LEFT:
                         return
 
-            
+    def ranking_draw(self,screen):
+        try:
+            print("Abiertos lectura 1")
+            file_scores_r = open("Ranking/scores.txt", 'r')
+            file_names_r = open("Ranking/names.txt", 'r')
+        except FileNotFoundError:
+            self.draw_text("No scores yet", WHITE, WIDTH / 5 * 2, HEIGHT / 3 + 100, screen, True)
+            return
+
+        scores = []
+        names = []
+        k=0
+        txt=""
+
+        for lines in file_scores_r.readlines():  # WE STORE THE POINTS
+            scores += [lines]
+
+        for lines in file_names_r.readlines():  # WE STORE THE NAMES
+            names += [lines]
+
+        # CLEAN THE LISTS
+        for i in range(0, len(names)):
+            names[i] = names[i][:len(names[i]) - 1]
+        print(names)
+
+        for i in range(0, len(scores)):
+            scores[i] = int(scores[i][:len(scores[i]) - 1])
+        print(scores)
+        # close the files
+        file_scores_r.close()
+        file_names_r.close()
+        if len(scores)<=3:
+            k=len(scores)
+        else:
+            k=Game.top_ranking
+        for i in range(0,k):
+            txt=names[i]+" - "+str(scores[i])
+            print(txt)
+            print(k)
+            self.draw_text(txt, WHITE, WIDTH / 4, HEIGHT / 3 + 100*i, screen, True)
+        pygame.display.flip()
+        #self.draw_text("Mouse pointer - Rotate the hero", 40, WHITE, WIDTH / 4, HEIGHT / 3 + 100, screen, False)
+        #self.draw_text("Right mouse click - Shoot", 40, WHITE, WIDTH / 4, HEIGHT / 3 + 200, screen, False)
     
     def options_draw(self, screen):
         img_menu = pygame.image.load("menu_screen_game.jpg")
         screen.blit(img_menu, (0, 0))
         #self.draw_text("MENU", 100, RED, WIDTH/8, HEIGHT/5, screen,True)
         if self.option==1:
-            self.draw_text("PLAY", 40, YELLOW, 40, HEIGHT/2, screen,True)
-            self.draw_text("TUTORIAL", 40, WHITE, WIDTH/3*2, HEIGHT/3 +100, screen,False)
-            self.draw_text("RANKING", 40, WHITE, WIDTH/3*2, HEIGHT/3 +200, screen,False)
+            self.draw_text("PLAY", YELLOW, menu_left_margin, HEIGHT/5*2, screen,True)
+            self.draw_text("TUTORIAL", WHITE, menu_left_margin, HEIGHT/5*2 +100, screen,False)
+            self.draw_text("RANKING", WHITE, menu_left_margin, HEIGHT/5*2 +200, screen,False)
         elif self.option==2:
-            self.draw_text("PLAY", 40, WHITE, WIDTH/3*2, HEIGHT/3, screen,False)
-            self.draw_text("TUTORIAL", 40, YELLOW, WIDTH/3*2, HEIGHT/3 +100, screen,True)
-            self.draw_text("RANKING", 40, WHITE, WIDTH/3*2, HEIGHT/3 +200, screen,False)
+            self.draw_text("PLAY", WHITE, menu_left_margin, HEIGHT/5*2, screen,False)
+            self.draw_text("TUTORIAL", YELLOW, menu_left_margin, HEIGHT/5*2 +100, screen,True)
+            self.draw_text("RANKING", WHITE, menu_left_margin, HEIGHT/5*2 +200, screen,False)
         else:
-            self.draw_text("PLAY", 40, WHITE, WIDTH/3*2, HEIGHT/3, screen,False)
-            self.draw_text("TUTORIAL", 40, WHITE, WIDTH/3*2, HEIGHT/3 +100, screen,False)
-            self.draw_text("RANKING", 40, YELLOW, WIDTH/3*2, HEIGHT/3 +200, screen,True)
+            self.draw_text("PLAY", WHITE, menu_left_margin, HEIGHT/5*2, screen,False)
+            self.draw_text("TUTORIAL", WHITE, menu_left_margin, HEIGHT/5*2 +100, screen,False)
+            self.draw_text("RANKING", YELLOW, menu_left_margin, HEIGHT/5*2 +200, screen,True)
         pygame.display.flip()
-    
+
+
     def wait_for_key_menu(self, screen):
-        waiting = True
-        global play_mode
-        while waiting:
+
+        while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    waiting = False
+
                     pygame.quit() # ends pygame
                     os._exit(0)
                     sys.exit()  # ends the program
@@ -148,21 +202,19 @@ class Game:
                     
     
     def wait_for_key_start(self):
-        waiting = True
-        global play_mode
-        while waiting:
+
+        while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    waiting = False
                     pygame.quit() # ends pygame
                     os._exit(0)
                     sys.exit()  # ends the program
                     return False
                 if event.type == pygame.KEYUP:
-                    waiting = False
+                    #pygame.event.clear()
                     return True
     
-    def draw_text(self, text, size, color, x, y, screen, selected):
+    def draw_text(self, text, color, x, y, screen, selected):
         if selected == True:
             text_surface = self.font_selected.render(text, True, color)
         else:
