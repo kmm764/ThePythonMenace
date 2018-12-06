c7f6f83b4f499b503275bf644011739e39289a36
Nerea Bejar <NBM883@hotmail.com>
2018-12-06 17:02:37 +0100

    references added

--- src/Hero.py
+++ src/Hero.py
@@ -132,5 +132,6 @@ class Hero(pygame.sprite.Sprite):
 def clamp(n, minn, maxn):
     """
         Function that limit the value of a given variable between a max and a min
+        Source:https://stackoverflow.com/questions/5996881/how-to-limit-a-number-to-be-within-a-specified-range-python
     """
     return max(min(maxn, n), minn)
