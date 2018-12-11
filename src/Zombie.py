6fc8b2ede6aa8e5753e9ae20934ef19afec5811f
Nerea Bejar <NBM883@hotmail.com>
2018-12-11 16:36:41 +0100

    superzombie

--- src/Zombie.py
+++ src/Zombie.py
@@ -52,3 +52,41 @@ class Zombie(Person):
         self.image = pygame.transform.rotate(Zombie.zombie_IMG, self.angle)
         self.rect = self.image.get_rect(center=self.rect.center)
         self.setPos(t)
+
+
+class SuperZombie(Zombie):
+    zombie_IMG = pygame.image.load('img/zombies/bigZombie.png')
+    life_bar_img = ["life_bar_empty_zombie.png", "life_bar_half_zombie.png", "life_bar_full_zombie.png"]
+    for b in range(len(life_bar_img)):
+        life_bar_img[b] = pygame.image.load("img/effects/life_bar/superzombie/"+life_bar_img[b])
+
+    def __init__(self, x, y):
+        super().__init__(x, y)
+        self.image = SuperZombie.zombie_IMG
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.lives = 3
+        self.lives_img = SuperZombie.life_bar_img[self.lives - 1]
+        self.img_height = 48
+        self.img_width = 40
+        self.pos_max_x = WIDTH - self.img_width
+        self.pos_max_y = HEIGHT - self.img_height
+
+    def updates_life(self, screen):
+        self.lives -= 1
+        self.lives_img = SuperZombie.life_bar_img[self.lives - 1]
+        screen.blit(self.lives_img, self.rect.midtop)
+        if self.lives == 0:
+            self.kill()
+
+    def life_bar_display(self, screen):
+        screen.blit(self.lives_img, self.rect.midtop)
+
+
+    def update(self, positionHero, t):
+        self.setAngle(positionHero)
+        self.rot = (self.rot + self.rot_speed * t) % 360
+        self.image = pygame.transform.rotate(SuperZombie.zombie_IMG, self.angle)
+        self.rect = self.image.get_rect(center=self.rect.center)
+        self.setPos(t)
